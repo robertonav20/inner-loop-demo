d5902c22ab7fd46819ceb8f6625674f38dbf5c04
roberto.nav20 <roberto.nav20@gmail.com>
2024-01-25 00:31:33 +0100

    Add assertions on test

--- order-service/src/main/java/inner/loop/demo/ApiController.java
+++ order-service/src/main/java/inner/loop/demo/ApiController.java
@@ -1,9 +1,13 @@
 package inner.loop.demo;
 
+import java.net.URI;
+import java.net.http.HttpResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -19,11 +23,16 @@ public class ApiController {
     }
 
     @PutMapping("/order/{quantity}")
-    public Integer order(@PathVariable int quantity) {
+    public ResponseEntity<Integer> order(@PathVariable int quantity) {
         UriComponents uriComponents =
                 UriComponentsBuilder.newInstance().scheme("http").host("warehouse-service")
                         .port("8001").path("/stock/{quantity}").buildAndExpand(quantity);
 
-        return restClient.put().uri(uriComponents.toUri()).retrieve().body(Integer.class);
+        try {
+            return ResponseEntity.created(uriComponents.toUri()).body(
+                    restClient.put().uri(uriComponents.toUri()).retrieve().body(Integer.class));
+        } catch (HttpClientErrorException e) {
+            return ResponseEntity.badRequest().body(-1);
+        }
     }
 }
